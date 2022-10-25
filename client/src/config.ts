a02ddf2e81306343505131fa0506a03edd6fda02
DemzyMaam <udemef.udoh@gmail.com>
2022-10-25 11:47:00 +0100

    Project4

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'v5q3ukb720';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-adbrrbld3kwrrdnj.us.auth0.com',
+  clientId: 'meiKcToAv5aDiQIpJTciWIGSfwnHgyKv',
+  callbackUrl: 'http://localhost:3000/callback'
+};
